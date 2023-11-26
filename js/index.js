7242408f84ab8ba316197dd5ac077c74897f3bfa
goziyev <goziyevmuhammadgozi@gmail.com>
2023-11-26 21:05:21 +0500

    task completed

---
+++ js/index.js
@@ -0,0 +1,108 @@
+// 1-misol
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+// if(a > b){
+//     console.log(
+//         a ** 2
+//     );
+// }
+// else{
+//     console.log(
+//         b ** 2
+//     );
+// }
+
+
+// 2-misol
+// let a = +prompt("1-sonni kiriting: ");
+// let b = +prompt("2-sonni kiriting: ");
+// let c = +prompt("3-sonni kiriting: ");
+
+// let max = a;
+// if (max < b) {
+//     if(b > c){
+//     max = b
+//     }
+// }
+// if(max < c){
+//     if(c > b){
+//         max = c
+//     }
+// }   
+// console.log(max);
+
+// 3-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// if(a % 2 == 0){
+//     console.log("Juft");
+// }
+// else{
+//     console.log("Toq");
+// }
+
+// 4-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// if(a % 5 == 0){
+//     console.log("Rost");
+// }
+// else{
+//     console.log("Yolg'on");
+// }
+
+// 5-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// let b;
+// b = (a % 10) / 10;
+// a = a / 10 - b;
+// console.log(a);
+
+// 6-misol
+// let a = +prompt("Marhamat son kiriting: ");
+// let b = ((a % 10) / 10 *10);
+// console.log(b);
+
+// 7-misol 
+// let a = +prompt("Marhamat son kiriting: ");
+// let b = ((a % 10) / 10);
+// let c = a / 10 - b;
+// let d = b*10;
+// if (c > d) {
+//     console.log("Rost");
+// }
+// else{
+//     console.log("Yolg'on");
+// }
+
+// 8-misol 
+// let a = +prompt("Marhamat son kiriting: ");
+// let b = ((a % 10) / 10 *10);
+// if(b % 2 == 0){
+//     console.log("Rost");
+// }
+// else{
+//     console.log("Yolg'on");
+// }
+
+// 9-misol 
+//  let a = +prompt("Marhamat son kiriting: ");
+//  let b = ((a % 100) / 100);
+//  let d =(( a / 100) - b );
+//  console.log(d);
+
+// 10-misol 
+// let a = +prompt("Marhamat son kiriting: ");
+// let birlik = (((a % 10) / 10 )*10);
+// let onlik = (((a % 100 ) - birlik )/10);
+//  let yuzlik =(( a / 100) - (a % 100) / 100) ;
+// let max = yuzlik;
+// if (max < onlik) {
+//     if(onlik > birlik){
+//     max = onlik
+//     }
+// }
+// if(max < birlik){
+//     if(birlik > onlik){
+//         max = birlik
+//     }
+// }
+// console.log(max);
